6ba30143da5a9cb3399e25746f169f90d82eca91
SIVAPRAKASH <152249009+Siva9943@users.noreply.github.com>
2026-09-23 12:35:10 +0530

    Fix the signup column to intergrate in front end

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -40,7 +40,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2', 'first_name', 'last_name')
+        fields = ('username', 'email', 'password', 'password2')
 
     def validate_email(self, value):
         if User.objects.filter(email__iexact=value).exists():
